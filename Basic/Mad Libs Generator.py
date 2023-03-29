2fb634185488d7f26ba9058602d6c207838b0df2
GorobecD <finerlo19@gmail.com>
2023-03-29 16:25:19 +0300

    Mad Libs Generator

---
+++ Basic/Mad Libs Generator.py
@@ -0,0 +1,16 @@
+color = input("Enter the color: ")
+adjective1 = input("Enter the adjective: ")
+time = input("Enter the time: ")
+adjective2 = input("Enter the adjective: ")
+place = input("Enter the place: ")
+food1 = input("Enter the food: ")
+food2 = input("Enter the food: ")
+verb = input("Enter the verb: ")
+noun = input("Enter the noun: ")
+number = input("Enter the number: ")
+
+
+print(f"Bats are so cool! They are {color}, {adjective1} animals which have wings. They like to fly around at {time} "
+      f"which makes some people scared of them. But bats are {adjective2}, and they don't want to hurt people. I have "
+      f"a pet bat that lives in {place}. I like to feed him {food1} and {food2}. He likes to {verb}. I am his favorite "
+      f"person, but he also likes {noun}. I want to convince my parents to get me {number} more bats.")
